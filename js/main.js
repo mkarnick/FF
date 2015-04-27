2ce6d33a1e599a3e51250159ca83544abe560b97
Matt Karnick <matt.karnick@gmail.com>
2015-04-26 20:16:14 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -56,8 +56,8 @@ myApp.controller('DashCtrl', ['$scope', 'Data_TeamList', 'Data_AllRosters','Data
 	$scope.signup = function() {
 
 		$scope.showAlert = 1;
-		$scope.signupresult.signupState = "info";
-		$scope.signupresult.signupMsg = "Checking availability...";
+		$scope.signupState = "info";
+		$scope.signupMsg = "Checking availability...";
 		SignupService.post($scope.inSignupSubname,$scope.inSignupLeagueId, $scope);
 		
 
@@ -81,11 +81,10 @@ myApp.controller('DashCtrl', ['$scope', 'Data_TeamList', 'Data_AllRosters','Data
 	
 myApp.controller('MainPageCtrl',  ['$scope', 'Service_Shared', 'SignupService', function($scope, Service_Shared, SignupService) {
 	window.MY_SCOPE = $scope;
-	var signupresult;
 	$scope.signup = function() {
 		$scope.showAlert = 1;
-		$scope.signupresult.signupState = "info";
-		$scope.signupresult.signupMsg = "Checking availability...";
+		$scope.signupState = "info";
+		$scope.signupMsg = "Checking availability...";
 		SignupService.post($scope.inSignupSubname,$scope.inSignupLeagueId, $scope);
 	
 		}
