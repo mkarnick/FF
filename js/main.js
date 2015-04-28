64b111d0a3f1f4b8e798ed24a39ee943c6a6770c
Matt Karnick <matt.karnick@gmail.com>
2015-04-27 22:04:35 -0400

    chat

--- js/main.js
+++ js/main.js
@@ -200,7 +200,7 @@ myApp.factory('Data_Chat', function($http) {
 	Data_Chat.post = function(inSub, inChatText, onSuccess) {
 		$http.post('chat/submit', {subname:inSub, author:'author', chatText:inChatText}).
 			  success(onSuccess).
-			  error(onFailure);
+			  error();
 	}
 	return Data_Chat;
 });
